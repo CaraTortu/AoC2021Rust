88b1f096976c7a2e7d007ef0280139a8edc8303c
CaraTortu <javierdj@jadk.com>
2022-11-01 15:23:53 +0000

    Day7

---
+++ Day7/B/src/main.rs
@@ -0,0 +1,66 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::vec;
+
+fn get_file(f: String) -> Vec<String> 
+{
+    let file = File::open(f).unwrap();
+    let reader = BufReader::new(file);
+
+    // Prepare Vector that contains the lines
+    let mut contents: Vec<String> = vec![];
+
+    // Read line by line
+    for line in reader.lines()
+    {
+        let line = line.expect("Error when reading file").parse::<String>();
+        contents.push(line.unwrap());
+    }
+
+    return contents;
+}
+
+fn solve(lines: &String) 
+{
+   let mut positions = [0i64; 2000];
+
+   for pos in lines.split(",")
+   {
+        positions[pos.parse::<usize>().unwrap()] += 1;
+   }
+
+   let mut min_res: i64 = -1;
+
+   for x in 0..2000
+   {
+        println!("{}% Done...", x/20);
+        let mut total_cost: i64 = 0;
+
+        for (pos, amount) in positions.into_iter().enumerate()
+        {
+            let mut added: i64 = 0;
+
+            for prev in 1..(pos as i64-x).abs()+1
+            {
+                added += prev;
+            }
+
+            total_cost += added * amount;
+        }
+
+        if total_cost < min_res || min_res == -1
+        {
+            min_res = total_cost;
+        }
+   }
+
+   println!("Minimum fuel {}", min_res);
+
+}
+
+fn main() {
+    let file: String = "input".to_string();
+    let lines: Vec<String> = get_file(file);
+
+    solve(lines.first().unwrap());
+}
